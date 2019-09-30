80aebdd36c11e8f000b059a3c27702188bca3aab
BuildTools <unconfigured@null.spigotmc.org>
2019-09-30 17:37:16 -0500

    something something graphQL

--- music/schema.py
+++ music/schema.py
@@ -4,6 +4,9 @@ from graphene_django.filter import DjangoFilterConnectionField
 
 from .models import Genre, Label, Artist, Album
 
+char_field_default_filters = ['exact', 'icontains', 'istartswith', 'iendswith', 'regex', 'iregex']
+date_default_filters = ['range', 'year', 'iso_year', 'month', 'day', 'week', 'week_day', 'quarter']
+
 
 class GenreNode(DjangoObjectType):
     class Meta:
@@ -11,28 +14,41 @@ class GenreNode(DjangoObjectType):
         filter_fields = ['name', 'artists', 'albums']
         interfaces = (relay.Node,)
 
+
+class LabelNode(DjangoObjectType):
+    class Meta:
+        model = Label
+        filter_fields = {
+            'id': ['exact'],
+            'name': char_field_default_filters,
+        }
+        interfaces = (relay.Node,)
+
+
 class ArtistNode(DjangoObjectType):
     class Meta:
         model = Artist
         filter_fields = {
             'id': ['exact'],
-            'name': ['exact', 'icontains', 'istartswith'],
-            'bio': ['exact', 'icontains', 'istartswith'],
+            'name': char_field_default_filters,
+            'bio': char_field_default_filters[:-4],
             'deans_list': ['exact'],
-            'labels': ['exact', 'icontains', 'istartswith'],
-            'genres': ['exact', 'icontains', 'istartswith']
+            'labels': char_field_default_filters,
+            'genres': char_field_default_filters
         }
         interfaces = (relay.Node,)
+
 
 class AlbumNode(DjangoObjectType):
     class Meta:
         model = Album
         filter_fields = {
             'id': ['exact'],
-            'name': ['exact', 'icontains', 'istartswith'],
-            'artists': ['exact', 'icontains', 'istartswith'],
-            'genres': ['exact', 'icontains', 'istartswith'],
-            'labels': ['exact', 'icontains', 'istartswith']
+            'name': char_field_default_filters,
+            'artists': char_field_default_filters,
+            'genres': char_field_default_filters,
+            'labels': char_field_default_filters,
+            'release_date': date_default_filters
         }
         interfaces = (relay.Node,)
 
@@ -41,9 +57,11 @@ class Query(ObjectType):
     genre = relay.Node.Field(GenreNode)
     all_genres = DjangoFilterConnectionField(GenreNode)
 
+    label = relay.Node.Field(LabelNode)
+    all_labels = DjangoFilterConnectionField(LabelNode)
+
     artist = relay.Node.Field(ArtistNode)
     all_artists = DjangoFilterConnectionField(ArtistNode)
 
     album = relay.Node.Field(AlbumNode)
     all_albums = DjangoFilterConnectionField(AlbumNode)
-
